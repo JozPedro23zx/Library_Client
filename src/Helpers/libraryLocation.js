49a8e3800338e2122278fdc1690dcb6feee79958
JozPedro23zx <josepedro23zx@gmail.com>
2021-10-29 12:03:55 -0300

    library location correction

--- src/Helpers/libraryLocation.js
+++ src/Helpers/libraryLocation.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 module.exports = async function selectLibrary(locationUser){
     if (locationUser){
         let response = await fetch(`${process.env.API_HOST}/getLibrary/:${locationUser.location}`)
-        let libraryId = response.json()
+        let libraryId = await response.json()
         
         return libraryId.library.id
     }
